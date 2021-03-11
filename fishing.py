5490219a6fbc976ff4d9f83f961afafba66fbbe9
Jayesh Seth <jayeshseth45@gmail.com>
2021-03-11 21:17:41 +0530

    made it lil bit more logical

--- fishing.py
+++ fishing.py
@@ -6,25 +6,30 @@ from .. import command, module
 
 fishing_chat = -1001463155229
 
+
 class FishingUtils(module.Module):
     name = "Fishing"
     disabled = False
     running = False
 
     @command.desc("Start Fishing!")
-    async def cmd_startFish(self, ctx: command.Context):
+    async def cmd_startfish(self, ctx: command.Context):
         self.running = True
         await ctx.respond("`-> Fishing Started!`")
         while self.running:
             await self.bot.client.send_message(fishing_chat, "/fish@CalsiBot")
             await asyncio.sleep(3)
-    
+
     @command.desc("Stop Fishing!")
     async def cmd_stopfish(self, ctx: command.Context):
-        self.running = False
-        await ctx.respond("`-> Fishing stopped!`")
+        if self.running == true:
+            self.running = False
+            await ctx.respond("`-> Fishing stopped!`")
+        else:
+            await ctx.respond("`You are not fishing currently`\n do it by 'startfish'")
 
     async def on_message(self, event: tg.events.NewMessage.Event) -> None:
-        if(event.is_group and str(event.chat_id) == str(fishing_chat)):
-            await self.bot.client.send_read_acknowledge(event.chat, event.message, clear_mentions = True)
-
+        if event.is_group and str(event.chat_id) == str(fishing_chat):
+            await self.bot.client.send_read_acknowledge(
+                event.chat, event.message, clear_mentions=True
+            )
