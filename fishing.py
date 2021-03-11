850ae665a3f555981567a448302af94d92ba3d59
jayesh <jayeshseth45@gmail.com>
2021-03-11 17:52:21 +0530

    fishing module added

---
+++ fishing.py
@@ -0,0 +1,30 @@
+import asyncio
+
+import telethon as tg
+
+from .. import command, module
+
+fishing_chat = -1001463155229
+
+class FishingUtils(module.Module):
+    name = "Fishing"
+    disabled = False
+    running = False
+
+    @command.desc("Start Fishing!")
+    async def cmd_startFish(self, ctx: command.Context):
+        self.running = True
+        await ctx.respond("`-> Fishing Started!`")
+        while self.running:
+            await self.bot.client.send_message(fishing_chat, "/fish@CalsiBot")
+            await asyncio.sleep(3)
+    
+    @command.desc("Stop Fishing!")
+    async def cmd_stopfish(self, ctx: command.Context):
+        self.running = False
+        await ctx.respond("`-> Fishing stopped!`")
+
+    async def on_message(self, event: tg.events.NewMessage.Event) -> None:
+        if(event.is_group and str(event.chat_id) == str(fishing_chat)):
+            await self.bot.client.send_read_acknowledge(event.chat, event.message, clear_mentions = True)
+
